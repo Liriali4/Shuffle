91a44d425ed450b4ea24c18d26fb8d4edfa57c06
Líria Li4 <lirialia51@gmail.com>
2023-03-16 10:37:50 +0100

    feito o shuffle e o reposicionamento do current song com o código do professor

--- src/App.js
+++ src/App.js
@@ -2,51 +2,59 @@ import './App.css';
 
 function App() {
 
-    const position = 0;
-    const currentSong = 1;
 
 
-    const playlist = [ 
-      {id: 567,title: "Meia Noite",artist: "Monsta",album: "Meia Noite",},
-      { id: 56, title: "Tóxico", artist: "NGA", album: "Tóxico",},
-      {id: 97, title: "Caixões Personalizados",artist: "Prodígio",album: "Prodigia-Te (Tuga Deluxe)",},
-      {id: 987,title: "Eu Não Danço",artist: "Eva Rapdiva",album: "Eu Não Danço",},
-      { id: 356,title: "Mobber",artist: "Gio-O, Xuxu Bower", album: "Mobber",}
-  ]   
+  const playlist = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 }
 
- /*function Shuffle(){
+    // {id: 567,title: "Meia Noite",artist: "Monsta",album: "Meia Noite",},
+    // { id: 56, title: "Tóxico", artist: "NGA", album: "Tóxico",},
+    // {id: 97, title: "Caixões Personalizados",artist: "Prodígio",album: "Prodigia-Te (Tuga Deluxe)",},
+    // {id: 987,title: "Eu Não Danço",artist: "Eva Rapdiva",album: "Eu Não Danço",},
+    // { id: 356,title: "Mobber",artist: "Gio-O, Xuxu Bower", album: "Mobber",}
+  ]
+    const currentSong = 2;
+    let click = false;
 
-      let shufflePlaylist = [...playlist];
-           
+  const shufflePlaylist = [...playlist]
 
-          shufflePlaylist.sort((a,b) => Math.random() - 0.5);
-          
-          playlist[currentSong] = shufflePlaylist[position]
+  console.log("Sem função",playlist)
 
-            console.log("shuffle==>", shufflePlaylist)
-            console.log("ordem==>", playlist)
-          
-  }*/
-const shufflePlaylist = [...playlist]
+  function Shuffle() {
+    if (click === false) {
+      click = true;
 
-console.log(shufflePlaylist)
+      let n = shufflePlaylist.length;
+      for (let i = n - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [shufflePlaylist[i], shufflePlaylist[j]] = [shufflePlaylist[j], shufflePlaylist[i]];
+      }
+        //shufflePlaylist.splice(0, 0,playlist[currentSong]);
+        const positionToDelete = shufflePlaylist.indexOf(playlist[currentSong])
+        console.log("positionToDelete:", positionToDelete)
 
-  function Shuffle(){
+        shufflePlaylist.splice(positionToDelete, 1);
+        shufflePlaylist.splice(0, 0, playlist[currentSong]);
+     
 
-         let n = shufflePlaylist.length;
-         for(let i = n -1; i>0;i--){
-          let j = Math.floor(Math.random()*(i+1));
-          [shufflePlaylist[i], shufflePlaylist[j]]= [shufflePlaylist[j], shufflePlaylist[i]];
-         }
+    } else {
+      click = false
+      
 
-    console.log("UNshuffle",playlist)  
-    console.log("shuffle",shufflePlaylist)  
-}
+    }
+
+    console.log("UNshuffle==>>", playlist)
+    console.log("shuffle==>>", shufflePlaylist)
+  }
 
   return (
     <div className="App">
-     <button onClick={ Shuffle}>shuffle</button>
-    
+      <button onClick={Shuffle}>shuffle</button>
+
     </div>
   );
 }
